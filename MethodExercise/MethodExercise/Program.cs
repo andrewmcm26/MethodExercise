06532853f22ba115dfaf889c67645b9d26bfedd0
Andrew McMahon <andrewmcm26@gmail.com>
2020-01-17 10:49:05 -0600

    Method Exercise #1

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color?");
+            var favColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var favAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite city?");
+            var favCity = Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Once upon a time there was a {favColor} {favAnimal} that " +
+                $"lived in {favCity}, and someone named {name} thought it was really cool.");
+        }
+    }
+}
