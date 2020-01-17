51ef19805c82bd0bd6e0b16ffe8fa19ff760e0ab
Andrew McMahon <andrewmcm26@gmail.com>
2020-01-17 12:32:06 -0600

    Method Exercise #1, mathematical operators

--- MethodExercise/MethodExercise/Program.cs
+++ MethodExercise/MethodExercise/Program.cs
@@ -6,16 +6,69 @@ namespace MethodExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your favorite color?");
-            var favColor = Console.ReadLine();
-            Console.WriteLine("What is your favorite animal?");
-            var favAnimal = Console.ReadLine();
-            Console.WriteLine("What is your favorite city?");
-            var favCity = Console.ReadLine();
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Once upon a time there was a {favColor} {favAnimal} that " +
-                $"lived in {favCity}, and someone named {name} thought it was really cool.");
+
+
+            //Beginning Exercise #1 - writing a story with variables
+
+            //Console.WriteLine("What is your favorite color?");
+            //var favColor = Console.ReadLine();
+            //Console.WriteLine("What is your favorite animal?");
+            //var favAnimal = Console.ReadLine();
+            //Console.WriteLine("What is your favorite city?");
+            //var favCity = Console.ReadLine();
+            //Console.WriteLine("What is your name?");
+            //var name = Console.ReadLine();
+            //Console.WriteLine($"Once upon a time there was a {favColor} {favAnimal} that " +
+            //    $"lived in {favCity}, and someone named {name} thought it was really cool.");
+
+            //double x = Sum(1, 2);
+
+            //Console.WriteLine(Sum(1, 2));
+            //Console.WriteLine(Mult(1, 2));
+            //Console.WriteLine(Dif(1, 2));
+            //Console.WriteLine(Div(1, 2));
+
+            Console.WriteLine("Enter your first number:");
+            double x = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            double y = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Your answers are (Addition Subtraction, Multiplication, Division):");
+
+            Console.WriteLine(Sum(x, y));
+            Console.WriteLine(Dif(x, y));
+            Console.WriteLine(Mult(x, y));
+            Console.WriteLine(Div(x, y));
+
         }
+            //Beginning Exercise #2 creating math operators
+            public static double Sum(double x, double y)
+            { 
+                 
+                return x + y;
+            }
+
+            
+
+            public static double Dif(double x, double y)
+            {
+                return x - y;
+            }
+          
+
+            public static double Mult(double x, double y)
+            {
+                return x * y;
+            }
+   
+
+            public static double Div(double x, double y)
+            {
+                return x / y;
+
+            }
+          
+        
+
+
     }
 }
